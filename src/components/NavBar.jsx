a22d1edab758f9bfcc88d5dc92aabe92eeeed86d
Yeshwant Thota <thotayeshwant@gmail.com>
2022-10-28 23:21:24 +0530

    Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { FaBars } from "react-icons/fa";
 const NavBar = () => {
   return (
     <div className="bg-stone-900 max-h-fit">
-      <div className="p-5 flex space-x-2 items-center space-x-8 max-w-fit">
+      <div className="p-5 flex space-x-2 items-center  max-w-fit">
         <div>
           <FaBars size={25} className="text-yellow-300" />
         </div>
